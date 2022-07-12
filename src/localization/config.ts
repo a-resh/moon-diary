fbc77c410e16eac98469fb6980aa7c8e2697b8f6
andrii.reshettsov <a.reshetcov@gmail.com>
2022-07-12 10:34:48 +0300

    add languages

--- src/localization/config.ts
+++ src/localization/config.ts
@@ -3,11 +3,37 @@ import * as Localization from 'expo-localization';
 
 import {en} from './translations/en';
 import {ru} from './translations/ru';
+import {uk} from './translations/uk';
+import {ar} from './translations/ar';
+import {af} from './translations/af';
+import {am} from './translations/am';
+import {az} from './translations/az';
+import {bg} from './translations/bg';
+import {bn} from './translations/bn';
+import {bs} from './translations/bs';
+import {ca} from './translations/ca';
+import {cs} from './translations/cs';
+import {cy} from './translations/cy';
+import {da} from './translations/da';
+import {de} from './translations/de';
 
 export const setI18nConfig = () => {
   i18n.translations = {
     en,
     ru,
+    uk,
+    az,
+    ar,
+    am,
+    af,
+    bg,
+    bn,
+    bs,
+    ca,
+    cs,
+    cy,
+    da,
+    de,
   };
   i18n.fallbacks = true;
   i18n.locale =
